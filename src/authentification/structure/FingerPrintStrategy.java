1d6631745d472a616ea05a54e6ced86a7b4dd238
atze119 <fynnjuranek@web.de>
2023-06-06 14:14:40 +0200

    added Strategy Pattern with AuthenticationService Zwischenstand

--- src/authentification/structure/FingerPrintStrategy.java
+++ src/authentification/structure/FingerPrintStrategy.java
@@ -2,7 +2,7 @@ package authentification.structure;
 
 public class FingerPrintStrategy implements Credential {
     @Override
-    public void authenticate() {
+    public void authenticate() { // TODO implement further
         System.out.println("You have chosen the Fingerprint-Strategy");
     }
 }
