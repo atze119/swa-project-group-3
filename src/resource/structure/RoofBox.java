171d330ae83d253bdcf9f51d4003b0e5d0f45353
atze119 <fynnjuranek@web.de>
2023-06-17 11:55:02 +0200

    added todo for question on monday 19.06

--- src/resource/structure/RoofBox.java
+++ src/resource/structure/RoofBox.java
@@ -1,6 +1,6 @@
 package resource.structure;
 
-public class RoofBox extends ResourceAdder {
+public class RoofBox extends ResourceAdder { // TODO in exam the name should be "SetTopBox" i think that makes no sense
     public RoofBox(Resource resource) {
         super(resource);
     }
