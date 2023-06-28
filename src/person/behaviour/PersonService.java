66a2a9b5e8867cbe57dfc71bd271adeccf71ec2a
atze119 <fynnjuranek@web.de>
2023-06-29 00:44:14 +0200

    changed Inputs from Person (true/false to Y/N)

--- src/person/behaviour/PersonService.java
+++ src/person/behaviour/PersonService.java
@@ -13,30 +13,39 @@ public class PersonService {
         PersonType personType= null;
         boolean correctData = false;
         Scanner sc = new Scanner(System.in);
-        boolean correctInput = false;
-        while (!correctInput) {
-            System.out.println("""
-            Please choose between the following two types:
-            Legal Person [L] | Natural Person [N]:
-            """);
-            String userInput = sc.next();
-            if (!userInput.equals("N") && !userInput.equals("L")) {
-                System.out.println("Please type a valid character!");
-            } else {
-                personType = userInput.equals("n") ? PersonType.NATURAL_PERSON : PersonType.LEGAL_PERSON;
-                correctInput = true;
-            }
-        }
         while (!correctData) { // as long as reset == false it will
+            boolean correctInput = false;
+            while (!correctInput) {
+                System.out.println("""
+                Please choose between the following two types:
+                Legal Person [L] | Natural Person [N]:
+                """);
+                String userInput = sc.next();
+                if (!userInput.equals("N") && !userInput.equals("L")) {
+                    System.out.println("Please input a valid character!");
+                } else {
+                    personType = userInput.equals("N") ? PersonType.NATURAL_PERSON : PersonType.LEGAL_PERSON;
+                    correctInput = true;
+                }
+            }
             PersonFactory personFactory = new PersonFactory();
             person = personFactory.create(personType);
             if (person != null) {
-                System.out.println();
-                System.out.println("Check if your data is correct");
-                person.printInformation();
-                System.out.println();
-                System.out.println("If anything is wrong type 'false' to reset your Data. If not type 'true' to go on.");
-                correctData = sc.nextBoolean(); // check to exit the loop
+                correctInput = false;
+                while (!correctInput) {
+                    System.out.println();
+                    System.out.println("Printing Information!");
+                    person.printInformation();
+                    System.out.println("Is your Data correct? Yes [Y] | No [N]");
+                    System.out.println();
+                    String validData = sc.next();
+                    if (!validData.equals("Y") && !validData.equals("N")) {
+                        System.out.println("Please input a valid character!");
+                    } else if (validData.equals("Y")) {
+                        correctData = true;
+                        correctInput = true;
+                    }
+                }
             } else {
                 break;
             }
--- src/person/structure/PersonFactory.java
+++ src/person/structure/PersonFactory.java
@@ -11,17 +11,35 @@ public class PersonFactory {
                 System.out.println("What is your First- and Lastname?");
                 String firstName = sc.next();
                 String lastName = sc.next(); // TODO implement if else to check if input is valid
-                System.out.println("Are you older than 21? (type 'true' or 'false')");
-                boolean age = sc.nextBoolean();
-                if (age == false) {
-                    System.out.println("Sorry you are to young for our service.");
-                    return null;
+                boolean correctInput = false;
+                boolean age = false;
+                while (!correctInput) {
+                    System.out.println("Are you older than 21? Yes [Y] | No [N]");
+                    String validAge = sc.next();
+                    if (!validAge.equals("Y") && !validAge.equals("N")) {
+                        System.out.println("Please input a valid character!");
+                    } else if (validAge.equals("Y")) {
+                        age = true;
+                        correctInput = true;
+                    } else {
+                        System.out.println("Sorry you are to young for our service.");
+                        return null;  // return null because we test if the return value (person) == null
+                    }
                 }
-                System.out.println("Do you have your driver's license since 3 years? (type 'true' or 'false')");
-                boolean licenseAge = sc.nextBoolean();
-                if (licenseAge == false) {
-                    System.out.println("Sorry thats not long enough for our service.");
-                    return null;
+                correctInput = false;
+                boolean licenseAge = false;
+                while(!correctInput) {
+                    System.out.println("Do you have your driver's license since 3 years? Yes [Y] | No [N]");
+                    String validLicenseAge = sc.next();
+                    if (!validLicenseAge.equals("Y") && !validLicenseAge.equals("N")) {
+                        System.out.println("Please input a valid character!");
+                    } else if (validLicenseAge.equals("Y")) {
+                        licenseAge = true;
+                        correctInput = true;
+                    } else {
+                        System.out.println("Sorry thats not long enough for our service.");
+                        return null;
+                    }
                 }
                 person = new NaturalPerson(firstName, lastName, age, licenseAge);
                 break;
@@ -33,7 +51,6 @@ public class PersonFactory {
             default:
                 throw new IllegalArgumentException("Unexpected Value: " + personType);
         }
-
         return person;
     }
 }
